da927788bd9cc37ac1baf75fdbf213f672c63f38
Emin Okic <emin.okic1729@gmail.com>
2025-08-19 12:09:43 -0500

    Making the appointments table cleaner

---
+++ d2d-follow-up-service/views/AppointmentsContainerView.swift
@@ -0,0 +1,23 @@
+//
+//  AppointmentsContainerView.swift
+//  d2d-studio
+//
+//  Created by Emin Okic on 8/19/25.
+//
+
+
+import SwiftUI
+
+struct AppointmentsContainerView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color(.systemGray6))
+                .shadow(color: .black.opacity(0.05), radius: 4, x: 0, y: 2)
+
+            AppointmentsSectionView()
+                .padding()
+        }
+        .frame(height: 400) // 3 rows + padding
+    }
+}
