ab499b110b27b40c35a0b198ff8deef6e21d0c0a
Emin Okic <emin.okic1729@gmail.com>
2025-12-13 14:45:16 -0600

    Big sexy pin drop

--- d2d-territory-management-service/views/MapSnapshotView.swift
+++ d2d-territory-management-service/views/MapSnapshotView.swift
@@ -13,20 +13,45 @@ import Contacts
 
 struct MapSnapshotView: View {
     let address: String
+
     @State private var region = MKCoordinateRegion()
+    @State private var coordinate: CLLocationCoordinate2D?
 
     var body: some View {
-        Map(coordinateRegion: $region)
-            .onAppear {
-                CLGeocoder().geocodeAddressString(address) { placemarks, _ in
-                    if let coord = placemarks?.first?.location?.coordinate {
-                        region = MKCoordinateRegion(
-                            center: coord,
-                            latitudinalMeters: 500,
-                            longitudinalMeters: 500
-                        )
-                    }
-                }
+        Map(
+            coordinateRegion: $region,
+            annotationItems: coordinate.map { [IdentifiableCoord(coord: $0)] } ?? []
+        ) { item in
+            MapAnnotation(coordinate: item.coord) {
+                Image(systemName: "mappin.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.red)
+                    .shadow(radius: 2)
+                    .offset(y: -12) // makes it feel like a dropped pin
             }
+        }
+        .onAppear {
+            geocode()
+        }
     }
+
+    private func geocode() {
+        CLGeocoder().geocodeAddressString(address) { placemarks, _ in
+            guard let coord = placemarks?.first?.location?.coordinate else { return }
+
+            DispatchQueue.main.async {
+                self.coordinate = coord
+                self.region = MKCoordinateRegion(
+                    center: coord,
+                    latitudinalMeters: 500,
+                    longitudinalMeters: 500
+                )
+            }
+        }
+    }
+}
+
+private struct IdentifiableCoord: Identifiable {
+    let id = UUID()
+    let coord: CLLocationCoordinate2D
 }
