a9719af1a66d15ea0f92647356cc38ed7dca33b6
Emin Okic <emin.okic1729@gmail.com>
2026-01-05 17:24:22 -0600

    Factoring out the ad impression model from the ad class

---
+++ d2d-ad-engine-service/models/AdImpression.swift
@@ -0,0 +1,13 @@
+//
+//  AdImpression.swift
+//  d2d-studio
+//
+//  Created by Emin Okic on 1/5/26.
+//
+
+import Foundation
+
+public struct AdImpression: Codable {
+    public let adId: String
+    public let timestamp: Date
+}
